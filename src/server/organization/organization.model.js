a0d5d2e755d5df71b25d5e0010f3e48ff5c647b4
Severiano Badajoz <sevebadajoz@gmail.com>
2019-01-09 13:38:26 -0800

    Add name attribute

--- src/server/organization/organization.model.js
+++ src/server/organization/organization.model.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const organizationSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   users: [{
     type: Schema.Types.ObjectId,
     ref: 'User',
