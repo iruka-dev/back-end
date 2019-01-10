baa2d2443d26908a48ea89ae0b7b4eb028772bb4
Severiano Badajoz <sevebadajoz@gmail.com>
2019-01-09 23:55:53 -0800

    Active the users route

--- src/index.route.js
+++ src/index.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const thingRoutes = require('./server/thing/thing.route');
-// const userRoutes = require('./server/user/user.route');
+const userRoutes = require('./server/user/user.route');
+
 const router = express.Router(); // eslint-disable-line new-cap
 
 // #TODO: Change to your model.
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 
 router.use('/things', thingRoutes);
 
