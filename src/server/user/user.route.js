571ab2050e4c594903012c9176d6014ae5f8b5d6
Severiano Badajoz <sevebadajoz@gmail.com>
2019-01-14 21:37:05 -0800

    Super stuck, store the issue save promises and save user when resolved

--- src/server/user/user.route.js
+++ src/server/user/user.route.js
@@ -23,6 +23,8 @@ router.route('/users');
 router.post('/', (req, res) => {
   // Remove existing data
   mongoose.connection.db.dropDatabase();
+  // Create a stack for saving promises
+  const promiseStack = [];
   // Get request for all stargazers of Ikura repo
   const getStargazers = octokit.activity.listStargazersForRepo.endpoint.merge({
     owner: 'iruka-dev',
@@ -35,37 +37,45 @@ router.post('/', (req, res) => {
       // Iterate over all stargazers
       stargazers.forEach((stargazer) => {
         const username = stargazer.login;
+        // Fetch a single user
         octokit.users.getByUsername({
           username,
         })
           .then((data) => {
+            // Grab that users organization
             let organizationName = data.data.company;
-            if (!organizationName) { return; }
+            // If the user doesn't have an organization stop
+            if (!organizationName) {
+              return;
+            }
+            // Remove whitespace
             organizationName = organizationName.toLowerCase().replace(/\s/g, '');
             // Remove @ from beginning of string if present
             if (organizationName.charAt(0) === '@') {
               organizationName = organizationName.substr(1);
             }
+            // Create user object
             const newUser = new User({
               username,
               issues: [],
               organization: organizationName,
             });
 
+
             const getRepos = octokit.repos.listForUser.endpoint.merge({
               username
             });
 
-
+            // Grab the user's repos
             octokit.paginate(getRepos)
               .then((repos) => {
                 repos.forEach((repo) => {
-                  console.log('Looking at repo:', username, repo.name);
+                  console.log(`Looking at repo: ${username}/${repo.name}`);
                   const getIssues = octokit.issues.listForRepo.endpoint.merge({
                     owner: username,
                     repo: repo.name,
                   });
-
+                  // Grab the repo's languages
                   octokit.repos.listLanguages({
                     owner: username,
                     repo: repo.name
@@ -73,10 +83,14 @@ router.post('/', (req, res) => {
                     const languageObj = result.data;
                     return Object.keys(languageObj);
                   }).then((languages) => {
+                    // Grab the repo's issues
                     octokit.paginate(getIssues)
                       .then((issues) => {
                         issues.forEach((issue) => {
+                          console.log(`Issue: ${issue.title}`);
+                          // Take the label names
                           const labels = issue.labels.map(a => a.name);
+                          // Create an issue
                           const newIssue = new Issue({
                             title: issue.title,
                             labels,
@@ -84,19 +98,29 @@ router.post('/', (req, res) => {
                             language: languages,
                           });
 
-                          newIssue.save()
+                          // Store the issue save promise in a list
+                          promiseStack.push(newIssue.save()
                             .then((savedIssue) => {
-                              newUser.issues.unshift(savedIssue);
-                            }).catch(err => console.error(err));
-                        });
-                        newUser.save().catch((err) => {
-                          console.error(err);
-                          console.error('Could not save user');
+                              console.log(`user: ${newUser.username}, issues: ${newUser.issues}`);
+                              // Insert the saved issue into the issues of the new user
+                              return newUser.issues.unshift(savedIssue);
+                            }).catch(err => console.error(err)));
                         });
                       }).catch(err => console.error(err));
                   }).catch(err => console.error(err));
                 });
               }).catch(err => console.error(err));
+            // After looking at all repos see that all the promises are resolved
+            Promise.all(promiseStack).then(() => {
+              console.log('Done with promises:', newUser);
+              // Then save the user
+              newUser.save().catch((err) => {
+                console.error(err);
+                console.log('ID:', newUser._id);
+                console.log('issues:', newUser.issues);
+                console.error('Could not save user');
+              });
+            });
           }).catch(err => console.error(err));
       });
       console.log('Refetched user data');
