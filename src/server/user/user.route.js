ae5b88c7a65a45453ae6980e2f30670239f8c7c8
Severiano Badajoz <sevebadajoz@gmail.com>
2019-05-13 22:46:44 -0700

    Change to graphql

--- src/server/user/user.route.js
+++ src/server/user/user.route.js
@@ -20,101 +20,84 @@ octokit.authenticate({
 router.route('/users');
 
 // POST: Update all of the users in the database
-router.post('/', (req, res) => {
+router.post('/', async (req, res)  =>  {
   // Remove existing data
   mongoose.connection.db.dropDatabase();
-  // Get request for all stargazers of Ikura repo
-  const getStargazers = octokit.activity.listStargazersForRepo.endpoint.merge({
-    owner: 'iruka-dev',
-    repo: 'iruka',
-  });
-
-  // Paginates, returning all stargazers instead of a page
-  octokit.paginate(getStargazers)
-    .then((stargazers) => {
-      // Iterate over all stargazers
-      stargazers.forEach((stargazer) => {
-        const username = stargazer.login;
-        // Fetch a single user
-        octokit.users.getByUsername({
-          username,
-        })
-          .then((data) => {
-            // Grab that users organization
-            let organizationName = data.data.company;
-            // If the user doesn't have an organization stop
-            if (!organizationName) {
-              return;
+  // Send request to GitHub graphql servers
+  try {
+    const data = await graphql(
+      `
+      {
+        repository(owner: "iruka-dev", name: "iruka") {
+          stargazers(first: 50) {
+            nodes {
+              login
+              company
+              repositories(last: 10) {
+                nodes {
+                  name
+                  languages(first: 3) {
+                    nodes {
+                      name
+                    }
+                  }
+                  issues(last: 10) {
+                    nodes {
+                      title,
+                      labels(first: 2) {
+                        nodes {
+                          name
+                          url
+                        }
+                      }
+                    }
+                  }
+                }
+              }
             }
-            // Remove whitespace
-            organizationName = organizationName.toLowerCase().replace(/\s/g, '');
-            // Remove @ from beginning of string if present
-            if (organizationName.charAt(0) === '@') {
-              organizationName = organizationName.substr(1);
-            }
-            // Create user object
-            const newUser = new User({
-              username,
-              issues: [],
-              organization: organizationName,
-            });
-
-
-            const getRepos = octokit.repos.listForUser.endpoint.merge({
-              username
-            });
-
-            // Grab the user's repos
-            octokit.paginate(getRepos)
-              .then((repos) => {
-                repos.forEach((repo) => {
-                  const getIssues = octokit.issues.listForRepo.endpoint.merge({
-                    owner: username,
-                    repo: repo.name,
-                  });
-                  // Grab the repo's languages
-                  octokit.repos.listLanguages({
-                    owner: username,
-                    repo: repo.name
-                  }).then((result) => {
-                    const languageObj = result.data;
-                    return Object.keys(languageObj);
-                  }).then((languages) => {
-                    // Grab the repo's issues
-                    octokit.paginate(getIssues)
-                      .then((issues) => {
-                        issues.forEach((issue) => {
-                          // Take the label names
-                          const labels = issue.labels.map(a => a.name);
-                          // Create an issue
-                          const newIssue = new Issue({
-                            title: issue.title,
-                            labels,
-                            link: issue.html_url,
-                            language: languages,
-                          });
-
-                          // Store the issue save promise in a list
-                          newIssue.save()
-                            .then((savedIssue) => {
-                              // Insert the saved issue into the issues of the new user
-                              newUser.issues.unshift(savedIssue);
-                              newUser.save().then(() => {
-                              }).catch((err) => {
-                                console.error(err);
-                                console.error('Could not save user:', newUser);
-                              });
-                            }).catch(err => console.error(err));
-                        });
-                      }).catch(err => console.error(err));
-                  }).catch(err => console.error(err));
-                });
-              }).catch(err => console.error(err));
-          }).catch(err => console.error(err));
+          }
+        }
+      }
+      `, {
+        headers: {
+          authorization: ``
+        }
+      }
+    )
+    
+    // Iterate over all stargazers
+    data.repository.stargazers.nodes.forEach((stargazer) => {
+      // Generate a new user in database
+      const newUser = new User({
+        username: stargazer.login,
+        issues: [],
+        organization: stargazer,
       });
-      console.log('Refetched user data');
+      // Iterate over all repos for said stargazer
+      stargazer.repositories.nodes.forEach((repo) => {
+        // Iterate over all issues for said repository
+        repo.issues.nodes.forEach((issue) => {
+          // Generate new issue
+          const newIssue = new Issue({
+            title: issue.title,
+            labels: issue.labels.nodes.map(issue => issue.name),
+            link: issue.url,
+            language: repo.languages.nodes.map(language => language.name),
+          });
+          // Save the issue into database
+          const savedIssue = await newIssue.save();
+          // Add the issue to the new user
+          newUser.issues.unshift(savedIssue);
+        })
+        // Save the new user to the database
+        newUser.save();
+      })
+    })
+    console.log('Refetched user data');
+  } catch(err) {
+    console.error(err);
+  }
       res.status(200).send();
-    }).catch(err => console.error(err));
 });
 
 // GET: Returns all the users
